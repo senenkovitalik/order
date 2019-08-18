8cf832fde8ea9206beb28aa6f1b7c9b909d8f79c
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-08-18 21:38:57 +0300

    feat: show employees inside table

--- frontend/src/components/pages/Unit/Unit.js
+++ frontend/src/components/pages/Unit/Unit.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import './Unit.css';
+
 export default class Unit extends React.Component {
   state = {
     unit: null
@@ -10,20 +12,36 @@ export default class Unit extends React.Component {
   render() {
     const unitName = this.state.unit ? this.state.unit.name : '';
     const employees = this.state.unit ? this.state.unit.employees : [];
+
     return (
       <div className='unit' style={{padding: '2rem'}}>
         <h2>Особовий склад підрозділу: {unitName}</h2>
-        <ul>
+        {
+          this.state.unit && <table>
+          <thead>
+          <tr>
+            <td>#</td>
+            <td>Вій. звання</td>
+            <td>ПІБ</td>
+            <td>Посада</td>
+          </tr>
+          </thead>
+          <tbody>
           {
-            employees
-              .sort((a, b) => b.rank.index - a.rank.index)
-              .map(employee => <li key={employee._id}>
-              {employee.rank.shortName} {employee.surname} {employee.name} {employee.patronymic}
-            </li>)
+            employees.sort((a, b) => b.rank.index - a.rank.index)
+            .map((employee, index) => <tr key={employee._id}>
+              <td>{index + 1}</td>
+              <td>{employee.rank.shortName}</td>
+              <td>{employee.surname} {employee.name} {employee.patronymic}</td>
+              <td>{employee.position.name}</td>
+            </tr>)
           }
-        </ul>
+          </tbody>
+        </table>
+        }
+
         <Link to={{
-          pathname: `${this.props.location.pathname}/order_chart/`,
+          pathname: `${this.props.location.pathname}/order_chart`,
           state: {unit: this.state.unit}
         }}>Графік чергування</Link>
       </div>
@@ -54,6 +72,9 @@ export default class Unit extends React.Component {
               rank {
                 index
                 shortName
+              }
+              position {
+                name
               }
               name
               surname
