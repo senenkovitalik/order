d1c053f33f118be7cfb0e8d24295b4ed14b3ffea
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-09-02 18:22:22 +0300

    fix: set preceding zero for days < 10

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,9 +1,10 @@
-const convertDate = (dateInMilliseconds) => {
-  if (!dateInMilliseconds) {
+// date may be Date or String (milliseconds)
+const convertDate = date => {
+  if (!date) {
     return '';
   }
-  const d = new Date(parseInt(dateInMilliseconds));
-  return `${d.getFullYear()}-${d.getMonth() < 10 ? `0${d.getMonth()}` : d.getMonth()}-${d.getDate()}`;
+  const d = date instanceof Date ? date : new Date(parseInt(date));
+  return `${d.getFullYear()}-${d.getMonth() < 10 ? `0${d.getMonth()}` : d.getMonth()}-${d.getDate() < 10 ? `0${d.getDate()}` : d.getDate()}`;
 };
 
 export default convertDate;
