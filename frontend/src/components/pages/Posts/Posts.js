85883b67b258bf2522bd039f1eba6a5763229a8e
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-11-07 18:49:39 +0200

    refactor: Post

--- frontend/src/components/pages/Posts/Posts.js
+++ frontend/src/components/pages/Posts/Posts.js
@@ -1,114 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import {Link, withRouter} from 'react-router-dom';
 
-class Posts extends React.Component {
-  state = {
-    postName: '',
-    posts: []
-  };
+function posts({posts, addPost, deletePost, location: {pathname}}) {
+  let inputRef;
 
-  addPost = e => {
+  function handleClick(e) {
     e.preventDefault();
-    const { unitId } = this.props.match.params;
-    const requestBody = {
-      query: `
-          mutation CreatePost($unitId: ID!, $postName:String!) {
-            createPost(unitId: $unitId, postName: $postName) {
-              _id
-              name
-            }
-          }
-        `,
-      variables: {
-        unitId,
-        postName: this.state.postName
-      }
-    };
+    addPost(inputRef.value);
+  }
 
-    axios.post('/graphql',
-      {},
-      {
-        baseURL: 'http://localhost:3001/',
-        params: requestBody,
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
+  return (
+    <div>
+      <h2>Бойові пости</h2>
+      <ul>
+        {
+          posts.map(({_id, name}) => <li key={_id}>
+            <Link to={`${pathname}/posts/${_id}`}>{name}</Link>
+            <button onClick={() => deletePost(_id)}>Delete</button>
+          </li>)
         }
-      })
-      .then(res => {
-        const post = res.data.data.createPost;
-        this.setState(prevState => ({
-          posts: [...prevState.posts, post]
-        }));
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  render() {
-    return (
-      <div style={{ padding: '2rem' }}>
-        <h2>Posts</h2>
-        <ul>
-          {
-            this.state.posts.map(({_id, name}) => <li key={_id}>
-              <Link to={`${this.props.location.pathname}/${_id}`}>{name}</Link>
-            </li>)
-          }
-        </ul>
-        <form>
-          <label>
-            New post{' '}<input type='text' name='postName' onChange={this.handleChange}/>
-          </label>
-          <button onClick={this.addPost}>Add post</button>
-        </form>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    const { unitId: id } = this.props.match.params;
-    const requestBody = {
-      query: `
-          query Unit($id: ID!) {
-            unit(id: $id) {
-              posts {
-                _id
-                name
-              }
-            }
-          }
-        `,
-      variables: {
-        id
-      }
-    };
-
-    axios.get('/graphql', {
-      baseURL: 'http://localhost:3001/',
-      params: requestBody,
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-      .then(res => {
-        const { posts } = res.data.data.unit;
-        this.setState({
-          posts
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+      </ul>
+      <form>
+        <label>
+          New post{' '}<input type='text' name='postName' ref={el => inputRef = el}/>
+        </label>
+        <button onClick={handleClick}>Add post</button>
+      </form>
+    </div>
+  );
 }
 
-export default Posts;
+export default withRouter(posts);
