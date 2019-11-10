c81b686819d584ba19d667e8b523da9440fd5423
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-11-10 17:45:28 +0200

    feat: disable add post button until user input

--- frontend/src/components/pages/Posts/Posts.js
+++ frontend/src/components/pages/Posts/Posts.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
-function posts({posts, addPost, deletePost, location: {pathname}}) {
-  let inputRef;
+class Posts extends React.Component {
+  state = {
+    postName: '',
+    canSubmit: false
+  };
 
-  function handleClick(e) {
+  handleClick = e => {
     e.preventDefault();
-    addPost(inputRef.value);
+    this.props.addPost(this.state.postName);
+  };
+
+  handleChange = e => {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value,
+      canSubmit: value.length > 3
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Бойові пости</h2>
+        <ul>
+          {this.props.posts.map(({_id, name}) => <li key={_id}>
+            <Link to={`${this.props.location.pathname}/posts/${_id}`}>{name}</Link>
+            <button onClick={() => this.props.deletePost(_id)}>Delete</button>
+          </li>)}
+        </ul>
+        <form>
+          <label>
+            New post{' '}
+            <input type='text'
+                   name='postName'
+                   onChange={this.handleChange}
+                   title={'Щось типу БП-000 чи 2БП-000'}/>
+          </label>
+          <button onClick={this.handleClick} disabled={!this.state.canSubmit}>Add post</button>
+        </form>
+      </div>
+    );
   }
 
-  return (
-    <div>
-      <h2>Бойові пости</h2>
-      <ul>
-        {
-          posts.map(({_id, name}) => <li key={_id}>
-            <Link to={`${pathname}/posts/${_id}`}>{name}</Link>
-            <button onClick={() => deletePost(_id)}>Delete</button>
-          </li>)
-        }
-      </ul>
-      <form>
-        <label>
-          New post{' '}<input type='text' name='postName' ref={el => inputRef = el}/>
-        </label>
-        <button onClick={handleClick}>Add post</button>
-      </form>
-    </div>
-  );
+  componentDidMount() {
+
+  }
 }
 
-export default withRouter(posts);
+export default withRouter(Posts);
