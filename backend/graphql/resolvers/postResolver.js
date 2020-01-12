45a124c3303be1c53b4a011dd127b3d2fd89ae0b
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-01-12 18:17:39 +0200

    fix: get duties for day

--- backend/graphql/resolvers/postResolver.js
+++ backend/graphql/resolvers/postResolver.js
@@ -3,18 +3,18 @@ const Duty = require('../../models/Duty');
 module.exports = {
   Post: {
     // month (from 0-11)
-    duties: async (parent, {year, month}) => {
-      const startDate = new Date(year, month, 1);
-      const endDate = new Date(year, month + 1, 0);
-      const startDateFormatted = startDate.toISOString().split('T')[0];
-      const endDateFormatted = endDate.toISOString().split('T')[0];
+    // day (from 1-31)
+    duties: async (parent, {year, month, day}) => {
+      const startDate = new Date(year, month, day ? day : 1);
+      const endDate = new Date(year, day ? month : month + 1, day ? day : 0);
+
       return await Duty.find({
         _id: {
           $in: parent.duties
         },
-        date: {
-          $gte: startDateFormatted,
-          $lte: endDateFormatted
+        date: day ? startDate.toISOString() : {
+          $gte: startDate.toISOString(),
+          $lte: endDate.toISOString()
         }
       });
     }
