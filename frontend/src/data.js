100ed7c516a7a2be4f657a4924dff56a611cc731
Vital <senenkovitalik@gmail.com>
2020-01-20 12:19:46 +0200

    refactor: remove unused

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,14 +1,3 @@
-export const addressData = [
-  { field: 'region', title: 'Область' },
-  { field: 'city', title: 'Місто' },
-  { field: 'district', title: 'Район' },
-  { field: 'urbanVillage', title: 'Селище міського типу' },
-  { field: 'village', title: 'Село' },
-  { field: 'street', title: 'Вулиця' },
-  { field: 'houseNumber', title: 'Будинок №' },
-  { field: 'apartmentNumber', title: 'Квартира №' }
-];
-
 export const monthes = [
   {name: "січень", days: 31},
   {name: "лютий", days: 28},
