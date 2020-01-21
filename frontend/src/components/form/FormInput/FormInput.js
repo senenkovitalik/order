123fa929f7938fee98d59160f560d3cdce1b0bf5
Vitaliy Senenko <senenkovitalik@gmail.com>
2020-01-21 14:07:26 +0200

    feat: configurable Form
    Form are generated by config object

--- frontend/src/components/form/FormInput/FormInput.js
+++ frontend/src/components/form/FormInput/FormInput.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function FormInput({ fieldName, type, name, value, title = '', handler }) {
   return (
-    <label>
-      <span className='form-field'>{fieldName}</span>
-      <input type={type} name={name} value={value} onChange={handler} title={title}/>
-    </label>
+    <div>
+      <label>
+        <span className='form-field'>{fieldName}</span>
+        <input type={type} name={name} value={value} onChange={handler} title={title}/>
+      </label>
+    </div>
   );
 }
